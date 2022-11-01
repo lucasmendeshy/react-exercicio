d1585e455391259b3e5b13ef075fafe23d433e6d
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-11-01 11:55:58 -0300

    Em andamento...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 const App = () => {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function getProdutos() {
+      try {
+        const resposta = await fetch("https://fakestoreapi.com/products");
+        // .then((res) => res.json())
+        // .then((json) => console.log(json));
+        const dados = await resposta.json();
+        setProdutos(dados);
+      } catch (error) {
+        console.log("Deu ruim! " + error.message);
+      }
+    }
+    getProdutos();
+  }, []);
+  /* console.log(produtos); Testando */
+
   return (
-    <section>
-      <h1>Template zerado!</h1>
-    </section>
+    <div>
+      <ul>
+        {produtos.map(({ id, title, price }) => {
+          <li key={id}></li>;
+        })}
+      </ul>
+    </div>
   );
 };
 export default App;
